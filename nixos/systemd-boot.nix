b3819bd4477bdcc93177aeb6d4fe5291a1bf2969
hot-steams417 <michal.gwozdz00@gmail.com>
2025-08-08 00:27:48 +0200

    fiufiu

--- nixos/systemd-boot.nix
+++ nixos/systemd-boot.nix
@@ -3,33 +3,29 @@
   boot = {
     bootspec.enable = true;
     loader = {
-      efi = {
-        canTouchEfiVariables = true;
-      };
-      # Disable GRUB
-      grub.enable = false;
-
+      efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
         consoleMode = "auto";
         configurationLimit = 8;
       };
     };
-  };
-  tmp.cleanOnBoot = true;
-  kernelPackages = pkgs.linuxPackages_latest;
 
-  # Silent boot
-  kernelParams = [
-    "quiet"
-    "splash"
-    "rd.systemd.show_status=false"
-    "rd.udev.log_level=3"
-    "udev.log_priority=3"
-    "boot.shell_on_fail"
-  ];
-  consoleLogLevel = 0;
-  initrd.verbose = false;
+    tmp.cleanOnBoot = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    # Silent boot
+    kernelParams = [
+      "quiet"
+      "splash"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "boot.shell_on_fail"
+    ];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+  };
 
   # To avoid systemd services hanging on shutdown
   systemd.extraConfig = "DefaultTimeoutStopSec=10s";
