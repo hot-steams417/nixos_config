4ed8f16b2336221afe86e0f70f3b1e09e2ae37d5
hot-steams417 <michal.gwozdz00@gmail.com>
2025-08-08 00:22:26 +0200

    typos

--- nixos/systemd-boot.nix
+++ nixos/systemd-boot.nix
@@ -5,33 +5,31 @@
     loader = {
       efi = {
         canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";  # Ensure this matches your EFI partition mount
       };
-      # Disable GRUB - CRUCIAL FOR DUAL BOOT
+      # Disable GRUB
       grub.enable = false;
-      
+
       systemd-boot = {
         enable = true;
         consoleMode = "auto";
         configurationLimit = 8;
-        };
       };
     };
-    tmp.cleanOnBoot = true;
-    kernelPackages = pkgs.linuxPackages_latest;
-
-    # Silent boot
-    kernelParams = [
-      "quiet"
-      "splash"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-      "boot.shell_on_fail"
-    ];
-    consoleLogLevel = 0;
-    initrd.verbose = false;
   };
+  tmp.cleanOnBoot = true;
+  kernelPackages = pkgs.linuxPackages_latest;
+
+  # Silent boot
+  kernelParams = [
+    "quiet"
+    "splash"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+    "boot.shell_on_fail"
+  ];
+  consoleLogLevel = 0;
+  initrd.verbose = false;
 
   # To avoid systemd services hanging on shutdown
   systemd.extraConfig = "DefaultTimeoutStopSec=10s";
